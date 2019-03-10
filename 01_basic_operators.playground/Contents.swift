9d452ba49882f2b1d25a408ca19d42d933c1bbd6
touchingdeveloper <dk@Dongkyunui-MacBookPro.local>
2019-03-10 13:26:36 +1100

    Add basic operators

---
+++ 01_basic_operators.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+var str = "Hello, playground"
+//str
+
+// variable 변수
+var number1 = 1
+number1 = 3
+number1 += 2
+
+// 상수
+let number2 = 2
+// number2 = 4
+
+// str = 5
+
+1 + 2
+10.0 / 2.5
+5 / 2
+5 % 2
+
+"abc" + "def"
